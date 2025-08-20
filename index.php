3a02bf8b106220e3de4afb40bbf2c1e3e8d42a33
kumaruday <aiokayuday@gmail.com>
2025-08-20 22:32:19 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,291 @@
+<!--------- Meta HTML Starts -------->
+<!DOCTYPE html>
+<html>
+
+  <head>
+    <title>SKL HIGH SCHOOL</title>
+    <link rel="icon" type="image/x-icon" href="../images/web.png">
+    <link rel="stylesheet" href="css/style.css">
+  </head>
+  <body>
+<!--------- Meta HTML End --------> 
+<!------ Header HTML Starts  ------->
+<div class="header">
+  <div class="container">
+    <b> NEWS : </b>
+    <marquee> My School: Intelligence plus curiosity-that is the root of true education. </marquee>
+    
+  </div>
+</div>
+<!------ Header HTML Ends ------->
+<!------ Design Logo And Main Menu Section HTML Startss ------->
+<div class="container">
+  <!-------------------------LOGO SECTION--------------------------->
+  <img src="images/l1.png" class="logo">
+  <!-------------------------MENU SECTION-------------------------->
+  <nav>
+  <div class="nav-container">
+
+<div class="links">
+  <ul>
+    <a href="./index.php">Home</a>
+    <a href="./about-us.php">About-us</a>
+    <a href="./login.php" class="btn">Login</a>
+  </ul>
+
+
+</div>
+
+</div>
+
+
+</div>
+</div>
+
+  </nav>
+</div>
+<!------ Design Logo And Main Menu Section HTML End ------->
+<!------ Slider HTML Starts ------->
+<div class="slider">
+  <img src="images/slider.png">
+</div>
+<!------ Slider HTML End ------->
+<!------ Campus, Events and Notice Board HTML Starts ------->
+<div class="main-section" style="background:#f0f3fa">
+  <div class="container">
+    <!--------------------------------CAMPUS NEWS SECTION ---------------------------->
+    <div class="event">
+      <h2 class="heading">School News</h2>
+      <div>
+        <marquee direction="up" scrollamount="7" style="height:340px;">
+          <ul>
+            <li>
+              <i>23-April-2025 :</i> Our school will officially reopen on June 12 following the summer vacation. We are excited to welcome students and staff back for a new academic terms. <img src="images/new.gif">
+            </li>
+            <li>
+              <i>9-April-2025 :</i> The final examinations will be held from April 9,2025 to April 20,2025. All students are advised to prepare accordingly and follow the exam schedule carefully. <img src="images/new.gif">
+            </li>
+            <li>
+              <i>23-April-2025 :</i> Check out the student-created science projects displayed in our school's annual fair. <img src="images/new.gif">
+            </li>
+            <li>
+              <i>23-April-2025 :</i> Lorem Ipsum is simply dummy text of the printing and typesetting industry. <img src="images/new.gif">
+            </li>
+            <li>
+              <i>23-April-2025 :</i> Lorem Ipsum is simply dummy text of the printing and typesetting industry. <img src="images/new.gif">
+            </li>
+            <li>
+              <i>23-April-2025 :</i> Lorem Ipsum is simply dummy text of the printing and typesetting industry. <img src="images/new.gif">
+            </li>
+            <li>
+              <i>23-April-2025 :</i> Lorem Ipsum is simply dummy text of the printing and typesetting industry. <img src="images/new.gif">
+            </li>
+          </ul>
+        </marquee>
+      </div>
+    </div>
+    <!----------------------------EVENT SECTION ----------------------->
+    <div class="event">
+      <h2 class="heading">Events</h2>
+      <div>
+        <ul>
+          <li>
+            <span class="event-date">14 <br> Jan </span> How our school celebrated Sankranti with kite flying, traditional food, and dance performances.
+          </li>
+          <li>
+            <span class="event-date">13 <br> Aug </span> Celebrating Bathukamma festival in school: A day of traditional music and dance.
+          </li>
+          <li>
+            <span class="event-date">15<br> Aug  </span> Independence Day Celebrations Cultural Highlights!".
+          </li>
+          <li>
+            <span class="event-date">14 <br> Nov </span>Childrens Day Carnival  A Day of Fun and Talent!".
+          </li>
+          <li>
+            <span class="event-date">05 <br> agu </span> Inter-School Sports Meet  Our Teams Shine Bright!"
+            Share photos, match results, and interviews with student-athletes.
+          </li>
+        </ul>
+      </div>
+    </div>
+    <!--------------------------------NOTICE BOARD SECTION ------------------------->
+    <div class="event">
+      <h2 class="heading">Notice Boards</h2>
+      <div>
+        <ul>
+          <li>
+            <img src="images/folder.png"> Stay tuned for upcoming school events, including sports day, cultural festivals, and annual functions! Mark your calendars!
+          </li>
+          <li>
+            <img src="images/folder.png"> Join us for an online workshop on AI&ML hosted by P.ARCHANA on 05-july. Don't miss out on this learning opportunity!
+          </li>
+         <!--------- <li>
+            <img src="images/folder.png"> Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+          </li>--------->
+          <li>
+            <img src="images/folder.png"> Reminder: The school will be closed on 20-04-2025 to 12-06-2025 for summer. Please plan accordingly.
+          </li>
+          <li>
+            <img src="images/folder.png"> Interested in joining a club? Our school has a variety of clubs to choose from. Visit the club room for more information.
+          </li>
+          <li>
+            <img src="images/folder.png"> Please remember to wear your mask and maintain social distancing to ensure the health and safety of all students and staff.
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</div
+<!------ Campus, Events and Notice Board HTML End ------->
+<!---------- About Us HTML Starts --------->
+<div class="main-section">
+  <div class="container about-us">
+    <div>
+      <h2 class="heading">About SLK HIGH SCHOOL</h2>
+      <h4>A Unique Heritage School .</h4>
+      <p> SLK High School is dedicated to academic excellence and all-round development. We provide a supportive environment that fosters learning, creativity, and discipline. With experienced faculty, modern facilities, and a focus on values, we empower students to become confident, responsible citizens. At SLK, we don’t just teach subjects — we shape futures with knowledge, integrity, and care.
+
+</p>
+    </div>
+    <img src="images/about-us.jpg">
+  </div>
+</div>
+<!---------- About Us HTML Ends --------->
+<!---------- School Info, Award and Certificate HTML Starts --------->
+<div class="main-section award" style="background:#f0f3fa">
+  <div class="container">
+    <!---------------- SCHOOL INFO SECTION --------------->
+    <div class="event">
+      <h2 class="heading">School Info</h2>
+      <div>
+        <h3>My SLK HIGH SCHOOL</h3>
+        <p>
+          <b>Principal : </b> E.SREE LAKSHMI
+        </p>
+        <p>
+          <b>Address : </b> sriram nagar, Near CMR shopping mall
+        </p>
+        <p>
+          <b>City : </b> suryapet,telangana, India.
+        </p>
+        <p>
+          <b>Contact No. : </b> +91 91822xxx
+        </p>
+        <p>
+          <b>Email Id : </b>Contact@admin LabArtisan
+        </p>
+      </div>
+    </div>
+    <!---------------- SCHOOL AWARD SECTION --------------->
+    <div class="event">
+      <h2 class="heading">Our School Awards</h2>
+      <div>
+        <img src="images/award.jpg">
+      </div>
+    </div>
+    <!---------------- SCHOOL CERTIFICATE SECTION --------------->
+    <div class="event">
+      <h2 class="heading">My School Certificate</h2>
+      <div>
+        <img src="images/certificate.png">
+      </div>
+    </div>
+  </div>
+</div>
+<!---------- School Info, Award and Certificate HTML Ends --------->
+<!--------- Testimonial HTML Starts -------->
+<div class="main-section ">
+  <div class="container">
+    <h2 class="heading">What Parents Say About Our School</h2>
+    <div class="testimonial">
+      <div class="testimonial-text"> Parents appreciate SLK High School for its strong academics, dedicated teachers, and safe environment. They value the school’s focus on discipline, moral values, and student growth. Many say their children are more confident, responsible, and eager to learn since joining SLK, making it their trusted choice. </div>
+      <div class="testimonial-detail">
+        <div class="testimonial-img">
+          <img src="images/testimonial-1.jpg">
+        </div>
+        <div class="testimonial-name">
+          <h5>uday kumar</h5>
+          <p>Web Devloper</p>
+        </div>
+      </div>
+    </div>
+    <div class="testimonial">
+      <div class="testimonial-text"> SLK High School has truly made a difference in our children’s lives. The caring teachers, excellent facilities, and balanced focus on academics and extracurriculars give our kids the best foundation. We’re proud to be part of a school that builds both knowledge and strong character in every student.</div>
+      <div class="testimonial-detail">
+        <div class="testimonial-img">
+          <img src="images/testimonial-2.jpg">
+        </div>
+        <div class="testimonial-name">
+          <h5>Richa</h5>
+          <p>Web Devloper</p>
+        </div>
+      </div>
+    </div>
+    <div class="testimonial">
+      <div class="testimonial-text"> We are happy with the progress our children have made at SLK High School. The school maintains high academic standards while also encouraging creativity and teamwork. Teachers are approachable and supportive. It’s a place where students feel valued, motivated, and prepared for a successful future. </div>
+      <div class="testimonial-detail">
+        <div class="testimonial-img">
+          <img src="images/testimonial-3.jpg">
+        </div>
+        <div class="testimonial-name">
+          <h5>Jiya</h5>
+          <p>Web Devloper</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+<!--------- Testimonial HTML End --------> 
+<!--------- Gallery HTML Starts -------->
+<div class="main-section gallery" style="background:#f0f3fa">
+  <div class="container ">
+    <h2 class="heading">Our School Gallery</h2>
+    <img src="images/gallery-1.jpg">
+    <img src="images/gallery-2.jpg">
+    <img src="images/gallery-3.jpg">
+    <img src="images/gallery-4.jpg">
+    <img src="images/gallery-5.jpg">
+    <img src="images/gallery-6.jpg">
+    <img src="images/gallery-7.jpg">
+    <img src="images/gallery-8.jpg">
+  </div>
+</div>
+
+<head>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+
+
+<div class="footer">
+  <div class="container">
+    <div class="footer-sect">
+      <img class="footer-logo" src="images/ft2.png">
+    </div>
+    <div class="footer-sect">
+      <ul>
+        <a href="./index.PHP">Home</a>
+        <a href="./about-us.php">About-us</a>
+        <a href="./login.php" class="btn">Login</a>
+      </ul>
+    </div>
+    <div class="footer-sect">
+      <!-- Social Media Icons Section -->
+      <div class="social-media-icons">
+        <a href="https://facebook.com" target="_blank">
+          <i class="fa-brands fa-facebook-f social-icon"></i>
+        </a>
+        <a href="https://twitter.com" target="_blank">
+          <i class="fa-brands fa-twitter social-icon"></i>
+        </a>
+        <a href="https://instagram.com" target="_blank">
+          <i class="fa-brands fa-instagram social-icon"></i>
+        </a>
+        <a href="https://linkedin.com" target="_blank">
+          <i class="fa-brands fa-linkedin social-icon"></i>
+        </a>
+      </div>
+    </div>
+  </div>
+</div>
+</head>
+
+
